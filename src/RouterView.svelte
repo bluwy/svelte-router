bf5651e986b12c50f0da57ca2ecc20e4a19c7d03
bluwy <bjornlu.dev@gmail.com>
2020-08-21 17:49:25 +0800

    Use optional chain in script only

--- src/RouterView.svelte
+++ src/RouterView.svelte
@@ -7,6 +7,7 @@
   export let nextMatched: RouteRecord[] | undefined = undefined
 
   $: matched = nextMatched ?? $route.matched
+  $: component = matched[0]?.component
   $: redirect = matched[0]?.redirect
 
   let canRender = false
@@ -24,7 +25,7 @@
 </script>
 
 {#if !redirect || canRender}
-  <svelte:component this={matched[0]?.component}>
+  <svelte:component this={component}>
     {#if matched.length > 1}
       <svelte:self nextMatched={matched.slice(1)} />
     {/if}
