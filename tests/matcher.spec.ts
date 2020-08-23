49c419f01460e0c1b5c3037c87934917950e2489
bluwy <bjornlu.dev@gmail.com>
2020-08-23 22:32:24 +0800

    Update matcher test

--- tests/matcher.spec.ts
+++ tests/matcher.spec.ts
@@ -1,198 +1,63 @@
-import { RouterError } from '../src/error'
-import { matchRoute, routesToMatchers } from '../src/matcher'
+import { RouteMatcher } from '../src/matcher'
 
 describe('matcher', function () {
-  context('matching', function () {
-    it('should match a route', function () {
-      const matchers = routesToMatchers([{ path: '/foo' }, { path: '/bar' }])
-      const route = matchRoute('/bar', matchers)
-      expect(route.matched[0].path).to.equal('/bar')
-    })
-
-    it('should match the same sequence of route records', function () {
-      const matchers = routesToMatchers([
-        {
-          path: '/foo',
-          children: [
-            {
-              path: '/bar',
-              children: [{ path: '/baz' }]
-            }
-          ]
-        }
-      ])
-      const route = matchRoute('/foo/bar/baz', matchers)
-      expect(route.matched.length).to.equal(3)
-      expect(route.matched[0].path).to.equal('/foo')
-      expect(route.matched[1].path).to.equal('/bar')
-      expect(route.matched[2].path).to.equal('/baz')
-    })
-
-    it('should match nested routes', function () {
-      const matchers = routesToMatchers([
-        {
-          path: '/foo',
-          children: [{ path: '/bar' }]
-        }
-      ])
-      const route1 = matchRoute('/foo/bar', matchers)
-      const route2 = matchRoute('/foo', matchers)
-      expect(route1.matched[1].path).to.equal('/bar')
-      expect(route2.matched.length).to.equal(1)
-      expect(route2.matched[0].path).to.equal('/foo')
-    })
-
-    it('should be tolerant to oddly named paths', function () {
-      const matchers = routesToMatchers([
-        {
-          path: 'foo',
-          children: [{ path: 'bar/' }, { path: 'baz' }, { path: '' }]
-        }
-      ])
-      const route1 = matchRoute('/foo/bar', matchers)
-      const route2 = matchRoute('/foo/baz', matchers)
-      const route3 = matchRoute('/foo/bla/', matchers)
-      expect(route1.matched[1].path).to.equal('bar/')
-      expect(route2.matched[1].path).to.equal('baz')
-      expect(route3.params.wild).to.equal('/bla')
-    })
+  it('should match a route', function () {
+    const matcher = new RouteMatcher([{ path: '/foo' }, { path: '/bar' }])
+    const route = matcher.matchRoute('/bar')
+    expect(route?.matched[0].path).to.equal('/bar')
   })
 
-  context('catch-all', function () {
-    it('should create a default catch-all route if not defined', function () {
-      const matchers = routesToMatchers([{ path: '/foo' }, { path: '/bar' }])
-      const route = matchRoute('/hello', matchers)
-      expect(route).to.be.ok
-      expect(route.matched.length).to.equal(0)
-    })
-
-    it('should create a default nested catch-all route if not defined', function () {
-      const matchers = routesToMatchers([
-        {
-          path: '/foo',
-          children: [{ path: '/bar' }]
-        }
-      ])
-      const route = matchRoute('/foo/invalid', matchers)
-      expect(route).to.be.ok
-      expect(route.matched.length).to.equal(1)
-      expect(route.matched[0].path).to.equal('/foo')
-    })
-
-    it('should default catch-all should have wild param', function () {
-      const matchers = routesToMatchers([])
-      const route = matchRoute('/foo/bar/', matchers)
-      expect(route.params.wild).to.eq('/foo/bar')
-    })
+  it('should match the same sequence of route records', function () {
+    const matcher = new RouteMatcher([
+      {
+        path: '/foo',
+        children: [
+          {
+            path: '/bar',
+            children: [{ path: '/baz' }]
+          }
+        ]
+      }
+    ])
+    const route = matcher.matchRoute('/foo/bar/baz')
+    expect(route?.matched.length).to.equal(3)
+    expect(route?.matched[0].path).to.equal('/foo')
+    expect(route?.matched[1].path).to.equal('/bar')
+    expect(route?.matched[2].path).to.equal('/baz')
   })
 
-  context('parsing', function () {
-    it('should parse named parameters', function () {
-      const matchers = routesToMatchers([{ path: '/foo/:id' }])
-      const route = matchRoute('/foo/abc', matchers)
-      expect(route.params.id).to.equal('abc')
-    })
-
-    it('should parse path query', function () {
-      const matchers = routesToMatchers([{ path: '/foo' }])
-      const route = matchRoute('/foo?id=abc', matchers)
-      expect(route.query.id).to.equal('abc')
-    })
-
-    it('should parse path hash', function () {
-      const matchers = routesToMatchers([{ path: '/foo' }])
-      const route = matchRoute('/foo#abc', matchers)
-      expect(route.hash).to.equal('#abc')
-    })
-
-    it('should parse all named parameters, query and hash', function () {
-      const matchers = routesToMatchers([{ path: '/foo/:id' }])
-      const route = matchRoute('/foo/abc?def=ghi#jkl', matchers)
-      expect(route.params.id).to.equal('abc')
-      expect(route.query.def).to.equal('ghi')
-      expect(route.hash).to.equal('#jkl')
-    })
-
-    it('should preserve full path when matching route', function () {
-      const matchers = routesToMatchers([{ path: '/foo' }])
-      const route = matchRoute('/foo#abc', matchers)
-      expect(route.fullPath).to.equal('/foo#abc')
-    })
+  it('should match nested routes', function () {
+    const matcher = new RouteMatcher([
+      {
+        path: '/foo',
+        children: [{ path: '/bar' }]
+      }
+    ])
+    const route1 = matcher.matchRoute('/foo/bar')
+    const route2 = matcher.matchRoute('/foo')
+    expect(route1?.matched[1].path).to.equal('/bar')
+    expect(route2?.matched.length).to.equal(1)
+    expect(route2?.matched[0].path).to.equal('/foo')
   })
 
-  context('redirects', function () {
-    it('should handle redirects', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo' },
-        { path: '/bar', redirect: '/foo' }
-      ])
-      const route = matchRoute('/bar', matchers)
-      expect(route.path).to.equal('/foo')
-    })
+  it('should be tolerant to oddly named paths', function () {
+    const matcher = new RouteMatcher([
+      {
+        path: 'foo',
+        children: [{ path: 'bar/' }, { path: 'baz' }, { path: '*' }]
+      }
+    ])
+    const route1 = matcher.matchRoute('/foo/bar')
+    const route2 = matcher.matchRoute('/foo/baz')
+    const route3 = matcher.matchRoute('/foo/bla/')
+    expect(route1?.matched[1].path).to.equal('bar/')
+    expect(route2?.matched[1].path).to.equal('baz')
+    expect(route3?.params.wild).to.equal('/bla')
+  })
 
-    it('should handle catch-all redirects', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo' },
-        { path: '/*', redirect: '/foo' }
-      ])
-      const route = matchRoute('/bar', matchers)
-      expect(route.path).to.equal('/foo')
-    })
-
-    it('should handle nested redirects', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo' },
-        {
-          path: '/bar',
-          children: [
-            {
-              path: '/baz',
-              redirect: '/foo'
-            }
-          ]
-        }
-      ])
-      const route = matchRoute('/bar/baz', matchers)
-      expect(route.path).to.equal('/foo')
-    })
-
-    it('should prioritize child redirects over parent redirects', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo' },
-        { path: '/hello' },
-        {
-          path: '/bar',
-          redirect: '/hello',
-          children: [
-            {
-              path: '/baz',
-              redirect: '/foo'
-            }
-          ]
-        }
-      ])
-      const route = matchRoute('/bar/baz', matchers)
-      expect(route.path).to.equal('/foo')
-    })
-
-    it('should handle dynamic redirects', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo' },
-        {
-          path: '/bar/:id',
-          redirect: (to) => `/${to.params.id}`
-        }
-      ])
-      const route = matchRoute('/bar/foo', matchers)
-      expect(route.path).to.equal('/foo')
-    })
-
-    it('should throw RouterError if infinite redirect detected', function () {
-      const matchers = routesToMatchers([
-        { path: '/foo', redirect: '/bar' },
-        { path: '/bar', redirect: '/foo' }
-      ])
-      expect(() => matchRoute('/foo', matchers)).to.throw(RouterError)
-    })
+  it('should parse named parameters', function () {
+    const matcher = new RouteMatcher([{ path: '/foo/:id' }])
+    const route = matcher.matchRoute('/foo/abc')
+    expect(route?.params.id).to.equal('abc')
   })
 })
