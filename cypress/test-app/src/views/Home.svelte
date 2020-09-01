6443de0ffc36fe57678d75ccc26840338efdf02c
bluwy <bjornlu.dev@gmail.com>
2020-09-01 15:15:34 +0800

    Test route transitions in cypress

--- cypress/test-app/src/views/Home.svelte
+++ cypress/test-app/src/views/Home.svelte
@@ -1,1 +1,7 @@
-<h2>Home</h2>
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+</script>
+
+<div transition:fade={{ duration: 1000 }}>
+  <h2>Home</h2>
+</div>
--- cypress/test-app/src/views/Null.svelte
+++ cypress/test-app/src/views/Null.svelte
@@ -1,1 +1,7 @@
-<h2>404</h2>
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+</script>
+
+<div transition:fade={{ duration: 1000 }}>
+  <h2>404</h2>
+</div>
