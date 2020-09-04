b62a2f551a7ac74d98ed75c7bf87b791cd607bb7
bluwy <bjornlu.dev@gmail.com>
2020-09-04 17:15:03 +0800

    Update tests

--- __tests__/init-usage.spec.ts
+++ __tests__/init-usage.spec.ts
@@ -1,10 +1,10 @@
-import { initRouter } from '../src'
+import { initHashRouter, initPathRouter } from '../src'
 
 describe('init usage', () => {
   it('should throw if initRouter called more than once', () => {
     const call = () => {
-      initRouter({ mode: 'hash', routes: [] })
-      initRouter({ mode: 'hash', routes: [] })
+      initHashRouter([])
+      initPathRouter([])
     }
     expect(call).toThrow()
   })
